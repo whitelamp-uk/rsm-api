e26cfd16b63203e3ebc29368bce6cb084195d8f0
Mark Page <mark.page@burdenandburden.co.uk>
2021-06-14 11:03:11 +0000

    Moves _readme table to mandate SQL file

--- create_mandate.sql
+++ create_mandate.sql
@@ -1,7 +1,14 @@
 
+CREATE TABLE IF NOT EXISTS `_readme` (
+  `project` char(64),
+  `location` varchar(255) NOT NULL,
+  PRIMARY KEY (`project`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+INSERT IGNORE INTO `_readme` (`project`, `location`) VALUES
+('whitelamp-uk/rsm-api', 'https://github.com/whitelamp-uk/rsm-api.git');
+
 
 DROP TABLE IF EXISTS `rsm_mandate`;
-
 CREATE TABLE `rsm_mandate` (
   `DDRefOrig` varchar(64) CHARACTER SET ascii DEFAULT NULL,
   `Name` varchar(255) DEFAULT NULL,
