feba4ef863dd5eef8dd38ccecc062e24bbe2ce7b
Mark Page <mark.page@burdenandburden.co.uk>
2023-01-13 13:38:03 +0000

    Moves provider code from org to global config

--- config.example.php
+++ config.example.php
@@ -4,6 +4,7 @@ define ( 'BLOTTO_PAY_API_RSM',          '/some/rsm-api/PayApi.php'    );
 define ( 'BLOTTO_PAY_API_RSM_CLASS',    '\Blotto\Rsm\PayApi'          );
 define ( 'BLOTTO_PAY_API_RSM_SELECT',   'SELECT DISTINCT(`ClientRef`) AS `crf` FROM `rsm_mandate`' );
 
+define ( 'RSM_CODE',                    'RSM'                         ); // Provider code
 define ( 'RSM_URL',                     'https://rsm5.rsmsecure.com/ddcm/ddcmApi.php' );
 define ( 'RSM_USER',                    'my_rsm_api'                  );
 define ( 'RSM_PASSWORD',                '**********'                  );
